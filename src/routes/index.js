d601fc1fe7b2a0bd1b40bee24bf4a24986a3cc4b
Furkan Doğu <furkandogu2018@gmail.com>
2024-05-09 20:18:32 +0300

    category tamamlandı

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,5 +5,6 @@ const router = require("express").Router()
 router.use("/users", require("./user"))
 router.use("/tokens", require("./token"))
 router.use("/auth", require("./auth"))
+router.use("/categories", require("./category"))
 
 module.exports = router
