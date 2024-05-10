677b29cd601d8245e5d6419a8aeda11ea2df597e
Furkan Doğu <furkandogu2018@gmail.com>
2024-05-10 14:17:19 +0300

    cors yüklendi

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 
 const express = require("express")
 const app = express()
+const cors = require("cors")
 
 /* ------------------------------------------------------- */
 //! Required Modules:
 
 require("dotenv").config()
 const PORT = process.env.PORT || 8000
+
+app.use(cors())
 
 require("express-async-errors")
 
