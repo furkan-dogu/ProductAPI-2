f1b0bce2a40b0fa71ec59e8129bde38d5da665eb
Furkan Doğu <furkandogu2018@gmail.com>
2024-05-11 17:50:27 +0300

    düzenleme

--- src/routes/category.js
+++ src/routes/category.js
@@ -3,18 +3,16 @@
 const router = require("express").Router();
 
 const category = require("../controllers/category")
-const { isAdmin } = require("../middlewares/permissions")
-
-router.use(isAdmin)
+const permissions = require("../middlewares/permissions")
 
 router.route("/")
-    .get(category.list)
-    .post(category.create)
+    .get(permissions.isLogin, category.list)
+    .post(permissions.isAdmin, category.create)
 
 router.route("/:id")
-    .get(category.read)
-    .put(category.update)
-    .patch(category.update)
-    .delete(category.delete)
+    .get(permissions.isLogin, category.read)
+    .put(permissions.isAdmin, category.update)
+    .patch(permissions.isAdmin, category.update)
+    .delete(permissions.isAdmin, category.delete)
 
 module.exports = router
